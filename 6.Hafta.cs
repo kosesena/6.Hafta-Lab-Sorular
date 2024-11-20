573642ea9bf960ec89a14c70dd7a4f61bced7f91
Sena Köse <147408306+kosesena@users.noreply.github.com>
2024-11-21 00:16:18 +0300

    Update 6.Hafta.cs

--- 6.Hafta.cs
+++ 6.Hafta.cs
@@ -1,4 +1,6 @@
-/* Overloading ile ilgili ilk örneğimiz:
+/* 
+Overloading ile ilgili ilk örneğimiz:
+
 1. Matematiksel İşlemleri Çeşitlendiren Bir Fonksiyon
 - Aynı adla ama farklı parametrelerle toplam işlemi yapacak,
 - İlk sürümü iki tam sayıyı toplasın,
