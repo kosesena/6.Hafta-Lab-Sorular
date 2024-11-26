bdbdb453fd92498c11daf5ed6a0ebf79c6c1311e
Sena Köse <147408306+kosesena@users.noreply.github.com>
2024-11-27 01:09:23 +0300

    Add files via upload

---
+++ Enum/Program-12.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public enum HavaDurumu
+{
+    Sunny,  // Güneşli
+    Rainy,  // Yağmurlu
+    Cloudy, // Bulutlu
+    Stormy  // Fırtınalı
+}
+
+public class HavaTahmini
+{
+    // Hava durumuna göre tavsiye veren metot
+    public string Tavsiye(HavaDurumu durum)
+    {
+        switch (durum)
+        {
+            case HavaDurumu.Sunny:
+                return "Hava güneşli, güneş gözlüğü takmayı unutmayın!";
+            case HavaDurumu.Rainy:
+                return "Hava yağmurlu, yanınıza bir şemsiye alın!";
+            case HavaDurumu.Cloudy:
+                return "Hava bulutlu, hafif bir ceket giyebilirsiniz.";
+            case HavaDurumu.Stormy:
+                return "Hava fırtınalı, evde kalmanız tavsiye edilir!";
+            default:
+                return "Bilinmeyen hava durumu!";
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        HavaTahmini havaTahmini = new HavaTahmini();
+
+        // Hava durumları için tavsiye al
+        Console.WriteLine($"Sunny: {havaTahmini.Tavsiye(HavaDurumu.Sunny)}");
+        Console.WriteLine($"Rainy: {havaTahmini.Tavsiye(HavaDurumu.Rainy)}");
+        Console.WriteLine($"Cloudy: {havaTahmini.Tavsiye(HavaDurumu.Cloudy)}");
+        Console.WriteLine($"Stormy: {havaTahmini.Tavsiye(HavaDurumu.Stormy)}");
+    }
+}
