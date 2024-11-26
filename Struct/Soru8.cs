c49077005b7cab42355212b77f57620bb7b76ee2
Sena Köse <147408306+kosesena@users.noreply.github.com>
2024-11-27 00:47:21 +0300

    Update Soru8.cs

--- Struct/Soru8.cs
+++ Struct/Soru8.cs
@@ -2,8 +2,7 @@
 1. Zaman İşlemleri
 Bir struct oluşturun:
  Saat ve dakikayı tutacak şekilde tasarlansın (Hour ve Minute özellikleri).
- Kullanıcı bir zaman nesnesi oluşturduğunda, eğer girilen saat/dakika geçersizse
-otomatik olarak 0 yapılmalı.
+ Kullanıcı bir zaman nesnesi oluşturduğunda, eğer girilen saat/dakika geçersizse otomatik olarak 0 yapılmalı.
  Zamanın toplam dakika değerini döndüren bir metot ekleyin.
  İki zaman nesnesi arasındaki farkı (dakika olarak) hesaplayan bir metot ekleyin.
 */
