06a55efc6494ad9b948f7af1684db382618ef90d
Sena Köse <147408306+kosesena@users.noreply.github.com>
2024-11-28 10:46:02 +0300

    Update Soru2.cs

--- Overloading/Soru2.cs
+++ Overloading/Soru2.cs
@@ -1,3 +1,12 @@
+/*
+2. Farklı Şekillerin Alanını Hesaplayan Bir Fonksiyon
+Bir fonksiyon yazın:
+ Aynı adla ama farklı parametrelerle farklı şekillerin alanını hesaplasın.
+ İlk sürüm, bir karenin alanını hesaplasın (bir kenar uzunluğu verilerek).
+ İkinci sürüm, bir dikdörtgenin alanını hesaplasın (iki kenar uzunluğu verilerek).
+ Üçüncü sürüm, bir dairenin alanını hesaplasın (yarıçap verilerek).
+*/
+
 using System;
 
 class Program
